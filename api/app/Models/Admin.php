0feb28f6101ccc0683a9a3b957a9c8ff559e3394
root <root@september-rain.com>
2025-09-27 08:00:17 +0900

    Partner -> Adminに修正

--- api/app/Models/Admin.php
+++ api/app/Models/Admin.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Partner extends Authenticatable
+class Admin extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens;
